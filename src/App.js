7257c6f5ac63efbfbfaba49d013dd6f5937a028a
Uzair Suria <uzair.a.suria@outlook.com>
2020-11-17 12:22:24 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
 	return (
 		<>
-			<div className="App">COVID Tracker App</div>
+			<div className="App">
+				<h2>COVID TRACKER</h2>
+				<h5>The App is under development. Please visit later</h5>
+			</div>
 		</>
 	);
 }
