9c9d18feded403ee6bd16409d16cd04899e88d22
Uzair Suria <uzair.a.suria@outlook.com>
2020-11-17 20:05:15 +0500

    Wrapped App component in context provider component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import CovidDataStore from './context/CovidDataStore';
 
 ReactDOM.render(
-	<>
+	<CovidDataStore>
 		<App />
-	</>,
+	</CovidDataStore>,
 	document.getElementById('root')
 );
