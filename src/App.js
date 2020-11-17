4e99fb69306fdad28b4ddaa0f54e74ce0a4bc9d7
Uzair Suria <uzair.a.suria@outlook.com>
2020-11-17 20:04:20 +0500

    Added "Cards" and "CountrySelect" component to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import React from 'react';
 import './App.css';
+// import { covidContext } from './context/CovidDataStore';
+import Cards from './components/Cards';
+import CountrySelector from './components/CountrySelector';
 
 function App() {
+	// const {
+	// 	country,
+	// 	setCountry,
+	// 	confirmed,
+	// 	setConfirmed,
+	// 	recovered,
+	// 	setRecovered,
+	// 	deaths,
+	// 	setDeaths,
+	// 	countriesList,
+	// 	setCountriesList,
+	// 	dates,
+	// 	setDates,
+	// } = useContext(covidContext);
+
+	// console.log(`Using Context`, recovered['Antigua and Barbuda']);
 	return (
-		<>
-			<div className="App">
-				<h2>COVID TRACKER</h2>
-				<h5>The App is under development. Please visit later</h5>
-			</div>
-		</>
+		<div className="container">
+			<CountrySelector />
+			<Cards />
+		</div>
 	);
 }
 
