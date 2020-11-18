030c7f8b84002c47fef82a43a09857b20dd9f7f4
Uzair Suria <uzair.a.suria@outlook.com>
2020-11-18 10:21:49 +0500

    changed default selection

--- src/context/CovidDataStore.js
+++ src/context/CovidDataStore.js
@@ -10,7 +10,7 @@ const CovidDataStore = ({ children }) => {
 		deaths: `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv`,
 	};
 
-	const [country, setCountry] = useState('Pakistan');
+	const [country, setCountry] = useState('global');
 	const [confirmed, setConfirmed] = useState({});
 	const [recovered, setRecovered] = useState({});
 	const [deaths, setDeaths] = useState({});
